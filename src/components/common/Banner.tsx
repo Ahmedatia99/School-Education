41966214fe26f09d7079f2196283090bd465320c
Ahmedatia99 <atyaa629@gmail.com>
2025-08-19 05:37:28 +0300

    solve issues in style

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -3,7 +3,7 @@ import { FaArrowRight } from "react-icons/fa6";
 
 function Banner() {
   return (
-    <section className="w-full px-2 mt-1">
+    <section className="container mx-auto w-full px-2 mt-1">
       <div className="banner-cover p-2 sm:p-1 md:p-2 bg-no-repeat bg-cover flex items-center gap-2 sm:gap-3 justify-center border-2 border-dark rounded-lg font-medium text-sm sm:text-xs md:text-lg text-center shadow-lg">
         <span>Admission is Open, Grab your seat now</span>
         <FaArrowRight className="mt-1 sm:mt-0" />
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -24,8 +24,8 @@ const Navbar: React.FC = () => {
   const location = useLocation();
 
   return (
-    <header className="px-2">
-      <nav className="mt-2 w-full bg-white container mx-auto flex items-center justify-between border-2 border-dark rounded-xl h-16">
+    <header className="px-2 container mx-auto mb-10">
+      <nav className="mt-2 w-full bg-white  flex items-center justify-between border-2 border-dark rounded-xl h-16">
         {/* Logo */}
         <div className="flex items-center gap-2 bg-morning px-3 rounded-bl-xl h-full rounded-tl-xl">
           <img src={logo} alt="logo" className=" w-32 " loading="lazy" />
