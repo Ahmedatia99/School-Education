675446894b7cb8452d84df3a309811d8780232ff
Taha Ebrahim <tahsniper312@gmail.com>
2025-08-21 21:27:49 +0300

    finished gallery section with adding imgaes to assets

--- src/pages/Academics.tsx
+++ src/pages/Academics.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import StudentsLearn from "../components/Academics/StudentsLearn";
 import HeroAcademics from "../components/Academics/HeroAcademics";
 import SpecialFuture from "../components/Academics/SpecialFuture";
+import RoomGallery from "../components/Academics/RoomGallery";
 
 function Academics() {
   return (
     <div className="container px-3 mx-auto">
       <HeroAcademics />
-      <SpecialFuture/>
+      <SpecialFuture />
       <StudentsLearn />
+      <RoomGallery />
     </div>
   );
 }
