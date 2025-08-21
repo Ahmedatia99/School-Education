de05a306b745ecb0919ff458162b4218ce874aca
Ahmedatia99 <atyaa629@gmail.com>
2025-08-21 18:33:49 +0300

    add FAQ section with responsive and framer-motion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Hero from "./../components/home/Hero";
+import OurBenefits from "./../components/home/OurBenefits";
+import OurTestimonials from "./../components/home/OurTestimonials";
+import FrequentlyQuestions from "./../components/home/FrequentlyQuestions";
 function Home() {
   return (
     <main className="container mx-auto">
       <Hero />
+      <OurBenefits />
+      <OurTestimonials />
+      <FrequentlyQuestions />
     </main>
   );
 }
